8df46f0b46aedb138436d856beec3f65f08f74e7
Sommer Olson <37749455+Olsosom@users.noreply.github.com>
2020-10-03 13:38:30 -0500

    Added code for line, char, word, word length
    Added code to update line count, character count, word count, and a hashmap for frequency of word length.

--- FileReport.java
+++ FileReport.java
@@ -1,15 +1,47 @@
 import java.io.*;
+import java.util.*;
 
 class FileReport {
     public static void main(String[] args) {
         BufferedReader reader = null;
+        HashMap<Integer, Integer> wordLength = new HashMap<Integer, Integer>();
         String fName = "";
-        
+        int lineCount = 0;
+        int charCount = 0;
+        int letterCount = 0;
+        int figureCount = 0;
+        int otherCount = 0;
+        int wordCount = 0;
+
         try {
+            // Check if there is an argument
             if (args.length > 0) {
                 fName = args[0];
-                reader = new BufferedReader(new FileReader(fName));
             }
+
+            // Create BufferedReader for input file
+            File file = new File(fName);
+            FileInputStream fileStream = new FileInputStream(file);
+            InputStreamReader input = new InputStreamReader(fileStream);
+            reader = new BufferedReader(input);
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                lineCount++;
+                charCount += line.length();
+                String[] words = line.split(" ");
+                wordCount += words.length;
+
+                for (String word : words) {
+                    int curNumber = 0;
+                    if (wordLength.get(word.length()) != null)
+                        curNumber = wordLength.get(word.length());
+                    wordLength.put(word.length(), curNumber + 1);
+                }
+            }
+
+
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -20,7 +52,7 @@ class FileReport {
                     reader.close();
             }
             catch (IOException e2) {
-                System.out.println("Error in closing BufferedReader");
+                System.out.println("Error closing BufferedReader");
             }
         }
 
@@ -30,13 +62,20 @@ class FileReport {
             writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream("Output.txt"), "utf-8"));
 
+            // Print out file information
             writer.write("Filename: " + fName + "\r\n");
-            writer.write("Number of lines: " + "\r\n");
-            writer.write("Number of characters (total): " + "\r\n");
-            writer.write("Number of letters: " + "\r\n");
-            writer.write("Number of figures: " + "\r\n");
-            writer.write("Number of other characters: " + "\r\n");
-            writer.write("Number of words: " + "\r\n");
+            writer.write("Number of lines: " + lineCount + "\r\n");
+            writer.write("Number of characters (total): " + charCount + "\r\n");
+            writer.write("Number of letters: " + letterCount + "\r\n");
+            writer.write("Number of figures: " + figureCount + "\r\n");
+            writer.write("Number of other characters: " + otherCount + "\r\n");
+            writer.write("Number of words: " + wordCount + "\r\n");
+
+            // Print out contents of HashMap
+            for (Map.Entry<Integer, Integer> curEntry : wordLength.entrySet()) {
+                writer.write("Number of " + curEntry.getKey() + " letter words: " 
+                    + curEntry.getValue() + "\r\n");
+            }
         }
         catch (IOException e3) {
 
